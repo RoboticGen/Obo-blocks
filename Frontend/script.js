adf8e03d61d4df395253be33146813039b63729a
yasanthaniroshan <niroshanyi@gmail.com>
2024-04-10 14:45:26 +0530

    Added Minor Changes - Changed index.html - Changed script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,13 +1,22 @@
 const copyButton = document.getElementById('copy-button');
 const runcodeButton = document.getElementById('run-button');
+const runButtonIcon = document.getElementById('run-icon');
+const runButtonText = document.getElementById('run-text');
 const codeGenerated = document.getElementById('output');
 const terminal = document.getElementById('terminal-output');
-let pyodide;
+const loadingIcon  = document.createElement('i');
+loadingIcon.className = 'material-icons left';
+loadingIcon.innerHTML = 'cycle';
+
 
 async function runcode() {
     try {
+        runcodeButton.className = 'waves-effect white btn disabled';
+        runButtonText.innerHTML = 'Running...';
         let code = codeGenerated.innerHTML;
         let output = pyodide.runPython(code);
+        runcodeButton.className = 'waves-effect white btn';
+        runButtonText.innerHTML = 'Run';
     } catch (err) {
         console.error('Error running code:', err);
     }
@@ -34,8 +43,15 @@ runcodeButton.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
+        runcodeButton.className = 'waves-effect white btn disabled';
+        runButtonIcon.innerText = 'cloud_download';
+        runButtonText.innerHTML = 'Loading...';
+        runButtonIcon.style.color = 'yellow';
         pyodide = await loadPyodide();
         runcodeButton.className = 'waves-effect white btn'; // Enable run button after Pyodide is loaded
+        runButtonIcon.innerText = 'flag';
+        runButtonText.innerHTML = 'Run';
+        runButtonIcon.style.color = 'green';
         terminal.innerHTML += 'Python 3.10 \n>>>'
         pyodide.setStdout({ batched: (msg) => terminal.innerHTML += msg + '\n>>>'});
     } catch (err) {
